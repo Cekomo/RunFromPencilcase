313a1d6f7f0010b5feaacc5fb3dbffadee154af7
cekomo <cemil.sahin@ozu.edu.tr>
2023-04-01 14:32:04 +0300

    add pencil platforming mechanism

--- Assets/Scripts/PlayerShotController.cs
+++ Assets/Scripts/PlayerShotController.cs
@@ -1,15 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShotController : MonoBehaviour
 {
     [SerializeField] private GameObject pencilSource;
+    private readonly Vector3 pencilOffset = new(0.2f, 0.2f, 0f);
 
     private void Update()
     {
+        if (!Input.GetMouseButtonDown(0)) return;
         
+        Instantiate(pencilSource, transform.position + pencilOffset, Quaternion.identity);
     }
 
     
--- Assets/Scripts/ThrowableController.cs
+++ Assets/Scripts/ThrowableController.cs
@@ -1,16 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ThrowableController : MonoBehaviour
 {
-    void Update()
+    private Rigidbody2D _rbPencil;
+
+    private bool isStuck;
+
+    private Vector3 _direction;
+
+    private const float PENCIL_SPEED = 7.5f;
+
+    private void Start()
     {
-        // make it move with transform.position or with rigidbody
+        _rbPencil = GetComponent<Rigidbody2D>();
+        
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        _direction = (mousePos - transform.position).normalized;
+        
+        var rotation = Quaternion.LookRotation(Vector3.forward, _direction) * Quaternion.Euler(0f, 0f, -90f);
+        transform.rotation = rotation;
+    }
+
+    private void Update()
+    {
+        if (isStuck) return;
+        
+        _rbPencil.velocity = _direction * PENCIL_SPEED;
     }
     
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
+        if (!col.gameObject.CompareTag("Wall")) return;
+        
+        var colliderPencil = GetComponent<Collider2D>();
+        colliderPencil.isTrigger = false;
+        
+        isStuck = true;
+        _rbPencil.velocity = Vector2.zero;
+        _rbPencil.angularVelocity = 0f;
+        _rbPencil.isKinematic = true;
+        gameObject.layer = LayerMask.NameToLayer("Platform");
+
         // when collision happens, to stabilize the object, make it kinetic and usable as platform for player
     }
 }
