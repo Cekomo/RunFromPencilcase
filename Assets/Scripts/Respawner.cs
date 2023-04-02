6ee9fc9a674c4306aa5cdce6c16d30a2f631b1b5
cekomo <cemil.sahin@ozu.edu.tr>
2023-04-02 20:24:06 +0300

    show the revealed letters

--- Assets/Scripts/Respawner.cs
+++ Assets/Scripts/Respawner.cs
@@ -5,18 +5,21 @@ public class Respawner : MonoBehaviour
     [SerializeField] private GameObject pencilCase;
     private static readonly int Erasing = Animator.StringToHash("Erasing");
     [SerializeField] private Animator playerAnimator;
+    public static int CheckPointIndex;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         var objectName = gameObject.name;
         var checkPointChar = objectName[objectName.Length - 1];
-        var checkPointIndex = (int)checkPointChar - (int)'0';
+        CheckPointIndex = (int)checkPointChar - (int)'0';
 
-        if (!col.gameObject.CompareTag("Player") || RespawnManager.IsPortalReachedList[checkPointIndex]) return;
+        if (!col.gameObject.CompareTag("Player") || RespawnManager.IsPortalReachedList[CheckPointIndex]) return;
 
         playerAnimator.SetTrigger(Erasing);
         gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        RespawnManager.IsPortalReachedList[checkPointIndex] = true;
-        RespawnManager.PencilCaseLocations[checkPointIndex] = pencilCase.transform.position;
+        RespawnManager.IsPortalReachedList[CheckPointIndex] = true;
+        RespawnManager.PencilCaseLocations[CheckPointIndex] = pencilCase.transform.position;
+
+        UIManager.AreLettersCheckable = true;
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private RespawnManager respawnManager;
+    public static bool AreLettersCheckable;
+
+    [SerializeField] private GameObject wordPanel; 
+
+    private void Update()
+    {
+        if (!AreLettersCheckable) return;
+        
+        wordPanel.gameObject.transform.GetChild(Respawner.CheckPointIndex).transform.gameObject.SetActive(true);
+        
+        AreLettersCheckable = false;
+    }
 
     public void ReturnCheckpoint()
     {
