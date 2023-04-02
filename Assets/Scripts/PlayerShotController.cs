930972aa1109b5a022d5450c29166705490a9ac5
cekomo <cemil.sahin@ozu.edu.tr>
2023-04-02 06:08:34 +0300

    add throwing animation

--- Assets/Scripts/PlayerShotController.cs
+++ Assets/Scripts/PlayerShotController.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class PlayerShotController : MonoBehaviour
 {
+    [SerializeField] private Animator playerAnimator;
+    
     [SerializeField] private GameObject pencilSource;
     private readonly Vector3 pencilOffset = new(0.2f, 0.2f, 0f);
+    
+    private static readonly int Throwing = Animator.StringToHash("Throwing");
 
     private void Update()
     {
         if (!Input.GetMouseButtonDown(0)) return;
         
         Instantiate(pencilSource, transform.position + pencilOffset, Quaternion.identity);
+        playerAnimator.SetTrigger(Throwing);
     }
 
     
