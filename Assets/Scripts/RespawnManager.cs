c51ee1d0f5c305a38b75361b18f6414505e2e583
cekomo <cemil.sahin@ozu.edu.tr>
2023-04-02 16:52:01 +0300

    implement checkpoint system

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -7,7 +7,23 @@ public class RespawnManager : MonoBehaviour
     
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject pencilCase;
+    [SerializeField] private GameObject checkPoint;
+
+    public static bool[] IsPortalReachedList;
+    public static int CurrentPortalIndex;
+
+    public static Vector3[] PencilCaseLocations;
     
+    private void Start()
+    {
+        CurrentPortalIndex = -1;
+        IsPortalReachedList = new bool[7] { false, false, false, false, false, false, false };
+        PencilCaseLocations = new Vector3[7]
+        {
+            Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero
+        };
+    }
+
     private void Update()
     {
         if (!PencilcaseController.IsGameOver) return;
@@ -19,12 +35,19 @@ public class RespawnManager : MonoBehaviour
 
     private void SetCheckPointPositions()
     {
-        if (Respawner.IsPositionSaved)
+        for (var i = 6; i >= 0; i--)
         {
-            player.transform.position = checkPointSo.playerPosition;
-            pencilCase.transform.position = checkPointSo.pencilCasePosition;
+            if (!IsPortalReachedList[i]) continue;
+            CurrentPortalIndex = i;
+            break;
         }
-        else
+
+        if (CurrentPortalIndex == -1)
             SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        else
+        {
+            player.transform.position = checkPoint.transform.GetChild(CurrentPortalIndex).transform.position;
+            pencilCase.transform.position = PencilCaseLocations[CurrentPortalIndex];
+        }
     }
 }
--- Assets/Scripts/Respawner.cs
+++ Assets/Scripts/Respawner.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class Respawner : MonoBehaviour
 {
-    public CheckPointSO checkPointSO;
     [SerializeField] private GameObject pencilCase;
-
-    public static bool IsPositionSaved;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (!col.gameObject.CompareTag("Player") || IsPositionSaved) return;
-        checkPointSO.SetPositions(transform.position, pencilCase.transform.position);
-        print("Position saved!");
-        IsPositionSaved = true;
+        var objectName = gameObject.name;
+        var checkPointChar = objectName[objectName.Length - 1];
+        var checkPointIndex = (int)checkPointChar - (int)'0';
+        
+        if (!col.gameObject.CompareTag("Player") || RespawnManager.IsPortalReachedList[checkPointIndex]) return;
+
+        RespawnManager.IsPortalReachedList[checkPointIndex] = true;
+        RespawnManager.PencilCaseLocations[checkPointIndex] = pencilCase.transform.position;
+        
+        print("saved!");
     }
 }
