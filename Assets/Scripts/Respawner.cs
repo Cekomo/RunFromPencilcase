3acff23fd528c00981bbf706344a2cf9c57c9505
cekomo <cemil.sahin@ozu.edu.tr>
2023-04-02 19:11:39 +0300

    add tutorial scene

--- Assets/Scripts/Respawner.cs
+++ Assets/Scripts/Respawner.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 public class Respawner : MonoBehaviour
 {
     [SerializeField] private GameObject pencilCase;
+    private static readonly int Erasing = Animator.StringToHash("Erasing");
+    [SerializeField] private Animator playerAnimator;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         var objectName = gameObject.name;
         var checkPointChar = objectName[objectName.Length - 1];
         var checkPointIndex = (int)checkPointChar - (int)'0';
-        
+
         if (!col.gameObject.CompareTag("Player") || RespawnManager.IsPortalReachedList[checkPointIndex]) return;
 
+        playerAnimator.SetTrigger(Erasing);
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
         RespawnManager.IsPortalReachedList[checkPointIndex] = true;
         RespawnManager.PencilCaseLocations[checkPointIndex] = pencilCase.transform.position;
-        
-        print("saved!");
     }
 }
